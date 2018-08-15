0ada5e0862409bc355f0bb980a0f9183d880e7d8
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 12:22:45 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(i)
+  while i>0 
+    puts "#{i} SECOND(S)!"
+  end
+    "HAPPY NEW YEAR!"
+  
 end
+
+
+  #countdown
+    # outputs '<n> SECOND(S)!' string to STDOUT for each count (FAILED - 1)
+    # returns HAPPY NEW YEAR! (FAILED - 2)
+  #countdown_with_sleep
+    # should take at least 5 seconds to execute (FAILED - 3)
